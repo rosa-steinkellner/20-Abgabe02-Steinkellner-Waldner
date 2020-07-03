ceda2c2c610e751a398fa6bd52afe0857d3f987e
Rosa Steinkellner <rosasteinkellner@gmail.com>
2020-07-03 14:28:37 +0200

    Added a test class for StringQueue
    Some of the tests are still failing due to bugs in StringQueue

--- src/test/java/at/fhj/iit/StringQueueTest.java
+++ src/test/java/at/fhj/iit/StringQueueTest.java
@@ -1,12 +1,71 @@
 package at.fhj.iit;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Testing StringQueue Implementation")
 class StringQueueTest {
 
-   // TODO Write your own tests
+    private static final String FIRST = "Hohoho";
+    private static final String SECOND = "Antipasti";
 
+    //Setting up the List needed to test functionality
+    private StringQueue stringQueue;
+
+    @BeforeEach
+    void setup() {
+        stringQueue = new StringQueue(5);
+        stringQueue.offer(FIRST);
+        stringQueue.offer(SECOND);
+    }
+
+    @Test
+    @DisplayName("Testing offer()")
+    void testOffer() {
+        assertTrue(stringQueue.offer("Mutzi"));
+        stringQueue.offer("Sunny");
+        stringQueue.offer("Cloudy");
+        stringQueue.offer("Stormy");
+        assertFalse(stringQueue.offer("Angry"));
+    }
+
+    @Test
+    @DisplayName("Testing poll()")
+    void testPoll() {
+        assertEquals(FIRST, stringQueue.poll());
+    }
+
+    @Test
+    @DisplayName("Testing remove()")
+    void testRemove() {
+        stringQueue.remove();
+        assertEquals(SECOND, stringQueue.peek());
+    }
+
+    @Test
+    @DisplayName("Testing, if Exception gets thrown when removing")
+    void testRemoveException() {
+        stringQueue.remove();
+        stringQueue.remove();
+        assertThrows(NoSuchElementException.class, () -> stringQueue.remove());
+    }
+
+    @Test
+    @DisplayName("Testing element()")
+    void testElement() {
+        assertEquals(FIRST, stringQueue.element());
+    }
+
+    @Test
+    @DisplayName("Testing that an exception gets thrown when using element")
+    void testElementException() {
+        stringQueue.remove();
+        stringQueue.remove();
+        assertThrows(NoSuchElementException.class, () -> stringQueue.element());
+    }
 }
